c6c1c76bac32b730727f73ffb53191ef439d08ee
buramman <borthakurmanish26@gmail.com>
2021-05-26 15:58:41 +0530

    Update basic_bloom_filter.py

--- basic_bloom_filter.py
+++ basic_bloom_filter.py
@@ -24,21 +24,8 @@ import numpy as np
 bit_array = bitarray(10)
 bit_array.setall(0)
 
-b1 = mmh3.hash(str(654), 41) % 10 
-bit_array[b1] = 1
-b2 = mmh3.hash(str(654), 42) % 10 
-bit_array[b2] = 1
-b1 = mmh3.hash(str(654), 41) % 10
-b2 = mmh3.hash(str(654), 42) % 10
-
 
 # In[7]:
-
-
-if bit_array[b1] == 1 and bit_array[b2] == 1:
-    print("Probably in set")
-else:
-    print("Definitely not in set")
 
 
 # In[8]:
@@ -68,35 +55,25 @@ class BloomFilter:
 # In[9]:
 
 
-bf = BloomFilter(1000,3)
-
-bf.add(51)
-bf.add(432)
-bf.add(11)
-bf.add(2875)
-
-print(bf.lookup(234))
-print(bf.lookup(51))
-print(bf.lookup(2875))
-print(bf.lookup(2876))
+bf = BloomFilter(100,3)
 
 
 # In[13]:
 
 
-#adds an array of random integers of size n (input : size, output: data array)
-def addrandom(n):
+#adds ratio r of an array of random integers of size n to bloom filter bf(input : bf,size,ratio; output: data array)
+def addrandom(bf,n,r):
     data=np.empty(n,dtype=int)
     for i in range(0,n):
         data[i]=random.randint(0, 100000000)
-    for j in range(0,n//2):
+    for j in range(0,int(n*r)):
         bf.add(data[j]);
     return data
 
 
 # In[14]:
 
-addrandom(100)
+data=addrandom(bf,100,0.3)
 print(bf.lookup(data[60]))
 print(bf.lookup(data[25]))
 
@@ -104,17 +81,17 @@ print(bf.lookup(data[25]))
 # In[ ]
 
 
-'''Function inputs bloom filter, number array and number of entries from number array and outputs -1 if there is false negative, and otherwise false positive rates'''
-def fpr(bf,nums,pos):
-    for i in range(pos):
+#(Input:bloom filter,number array,ratio of positives; Output:(-1) for false negative, otherwise fpr)
+def fpr(bf,nums,r):
+    for i in range(int(len(nums)*r)):
         if(bf.lookup(nums[i])==False):
             return -1
     count = 0
-    for i in range(pos,len(nums)):
+    for i in range(int(len(nums)*r),len(nums)):
         if(bf.lookup(nums[i])==True):
             count+=1
     return count/len(nums)
 
-print(fpr(bf,data,50))
+print(fpr(bf,data,0.3))
 
-# %%
+
