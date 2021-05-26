f40177906102e39575a523be00587b2c7ceb00e6
AditiJain14 <67906321+AditiJain14@users.noreply.github.com>
2021-05-26 15:15:41 +0530

    Update basic_bloom_filter.py

--- basic_bloom_filter.py
+++ basic_bloom_filter.py
@@ -1,5 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+pip install bitarray
+
+
+# In[4]:
+
+
+pip install mmh3
+
+
+# In[12]:
+
+
 from bitarray import bitarray
 import mmh3
+import random
+import numpy as np
+
+
+# In[6]:
+
 
 bit_array = bitarray(10)
 bit_array.setall(0)
@@ -10,10 +34,19 @@ b2 = mmh3.hash(str(654), 42) % 10
 bit_array[b2] = 1
 b1 = mmh3.hash(str(654), 41) % 10
 b2 = mmh3.hash(str(654), 42) % 10
+
+
+# In[7]:
+
+
 if bit_array[b1] == 1 and bit_array[b2] == 1:
     print("Probably in set")
 else:
     print("Definitely not in set")
+
+
+# In[8]:
+
 
 class BloomFilter:
     
@@ -35,6 +68,10 @@ class BloomFilter:
                 return False
         return True
 
+
+# In[9]:
+
+
 bf = BloomFilter(1000,3)
 
 bf.add(51)
@@ -46,3 +83,27 @@ print(bf.lookup(234))
 print(bf.lookup(51))
 print(bf.lookup(2875))
 print(bf.lookup(2876))
+
+
+# In[13]:
+
+
+data=np.empty(100,dtype=int)
+for i in range(0,100):
+    data[i]=random.randint(0, 100000000)
+for j in range(0,50):
+    bf.add(data[j]);
+
+
+# In[14]:
+
+
+print(bf.lookup(data[60]))
+print(bf.lookup(data[25]))
+
+
+# In[ ]:
+
+
+
+
